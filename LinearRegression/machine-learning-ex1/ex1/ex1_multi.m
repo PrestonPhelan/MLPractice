1c9ba588e17106748f5e541cec8e993036949ea8
PrestonPhelan <preston.phelan@gmail.com>
2017-07-23 18:30:09 -0700

    Normal equation solution

--- LinearRegression/machine-learning-ex1/ex1/ex1_multi.m
+++ LinearRegression/machine-learning-ex1/ex1/ex1_multi.m
@@ -151,7 +151,8 @@ fprintf('\n');
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
 price = 0; % You should change this
-
+values = [1, 1650, 3];
+price = values * theta;
 
 % ============================================================
 
