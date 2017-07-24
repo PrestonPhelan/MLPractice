a5a694bf97308f0ef2582e120ef8fa9ed8a54949
PrestonPhelan <preston.phelan@gmail.com>
2017-07-23 17:27:35 -0700

    Compute cost, 1 feature

--- LinearRegression/machine-learning-ex1/ex1/computeCost.m
+++ LinearRegression/machine-learning-ex1/ex1/computeCost.m
@@ -5,14 +5,15 @@ function J = computeCost(X, y, theta)
 
 % Initialize some useful values
 m = length(y); % number of training examples
+costCoefficient = 1 / (2 * m);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
+J = sum(((X * theta) - y) .^ 2) * costCoefficient;
 
 
 
