cccb284bdfb56bd09466493711767955471d3a6a
PrestonPhelan <preston.phelan@gmail.com>
2017-07-27 23:39:42 -0700

    Part 2 - sigmoid function takes matrices

--- FirstSet/machine-learning-ex2/ex2/costFunction.m
+++ FirstSet/machine-learning-ex2/ex2/costFunction.m
@@ -7,9 +7,10 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
+h = sigmoid(X' * theta);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
@@ -19,7 +20,8 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
-
+% X
+h = sigmoid(X' * theta)
 
 
 
